8821b957cc1145afc8310ec681d73d3bed2c09d5
shrutip90 <shruti.p90@gmail.com>
2014-05-12 13:52:35 -0400

    Initial code

---
+++ Pages/js/scripts.js
@@ -0,0 +1,13 @@
+/**
+ * Main
+ *
+ * CGIS
+ * Thur May 8 2014
+ */
+;(function (window, document, $, undefined) {
+  'use strict';
+
+  $(document).ready(function () { 
+  });
+
+}(window, document, jQuery));
